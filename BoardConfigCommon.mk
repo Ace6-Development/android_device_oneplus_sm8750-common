d1d6b81a449a7752a75c7fcdd2d47ddb0bc17d13
NullCode1337 <contact.nullcode@proton.me>
2026-09-19 10:27:14 +0600

    sm8750-common: Ship kiwi_v2 alongside peach_v2 WLAN module variants
    Depends on commit
    https://review.lineageos.org/c/LineageOS/android_kernel_qcom_sm8750-modules/+/503165
    
    Change-Id: I8a752e1ac57627dd9ce4445a10e36ab3c9b372e8

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -146,7 +146,8 @@ TARGET_KERNEL_EXT_MODULES := \
     qcom/opensource/spu-kernel \
     qcom/opensource/video-driver \
     qcom/opensource/wlan/platform \
-    qcom/opensource/wlan/qcacld-3.0 \
+    qcom/opensource/wlan/qcacld-3.0/.peach_v2 \
+    qcom/opensource/wlan/qcacld-3.0/.kiwi_v2 \
     qcom/opensource/bt-kernel
 
 TARGET_KERNEL_EXT_MODULES += \
